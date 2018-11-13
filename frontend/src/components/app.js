63539403972845f42996dc6bba2f355db5c5b157
A-Game <javascript@adamheldring.com>
2018-11-13 17:04:56 +0100

    started react frontend

---
+++ frontend/src/components/app.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+class App extends React.Component {
+
+  render() {
+    return (
+      <div>
+        <h1 className="tempHeader">WATCH YOUR TONE</h1>
+      </div>
+    )
+  }
+
+}
+
+export default App
